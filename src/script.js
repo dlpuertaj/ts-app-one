0c8670db0ffefb03d244783c2b36335cb9e55194
David Leonardo <dlpuerta@gmail.com>
2024-09-27 16:29:05 -0500

    adding buttons using a button

--- src/script.js
+++ src/script.js
@@ -1,9 +1,31 @@
 "use strict";
 const displayArea = document.getElementById('display-area');
-const inputBox = document.getElementById('input-box');
-const button = document.getElementById('action-button');
-button.addEventListener('click', () => {
-    console.log('Button clicked! adding log in text area');
-    alert('Button clicked!');
-    displayArea.value += "Button was clicked\n";
-});
+const buttonContainer = document.getElementById('button-container');
+const button = document.getElementById('add-button');
+
+
+let buttonCount = 0;
+
+function addNewButton(){
+
+    console.log(`Adding a new button`);
+    buttonCount++;
+
+    const newButton = document.createElement('button');
+
+    newButton.textContent = `Button ${buttonCount}`;
+    newButton.id = `button-${buttonCount}`;
+
+    console.log(`New button id: ${newButton.id}`)
+    newButton.addEventListener('click', () => genericButtonEventListener(newButton.id) );
+
+    buttonContainer.appendChild(newButton);
+    
+}
+
+function genericButtonEventListener(customString){
+    console.log(`Button ${customString} clicked! adding log in text area`);
+    displayArea.value += `Button ${customString} was clicked\n`;
+}
+
+button.addEventListener('click', addNewButton);
