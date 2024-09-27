0f85fe7470857150eb59c51806407229ffe41aa2
David Leonardo <dlpuerta@gmail.com>
2024-09-27 09:54:16 -0500

    Button adding text to display area

--- src/script.js
+++ src/script.js
@@ -1,7 +1,9 @@
-const displeyArea = document.getElementById('displey-area');
+"use strict";
+const displayArea = document.getElementById('display-area');
 const inputBox = document.getElementById('input-box');
 const button = document.getElementById('action-button');
-
-button.addEventListener('click', () =>{
+button.addEventListener('click', () => {
+    console.log('Button clicked! adding log in text area');
     alert('Button clicked!');
+    displayArea.value += "Button was clicked\n";
 });
