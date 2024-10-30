18d1ef6a401bcbbfba6ea6fd40d6186b3668ec3b
David Puerta <dlpuertagmail.com>
2024-10-30 16:05:46 -0500

    Shoing data in popup

--- src/popup_script.js
+++ src/popup_script.js
@@ -2,34 +2,18 @@
 
 
 const saveButton = document.getElementById('save-button');
-//const deleteButton = document.getElementById('delete-button');
 const dateTextField = document.getElementById('popup-input-date');
 const dataTextField = document.getElementById('popup-input-text');
 
-
-
-function saveButtonEventListener() {
-    // get the data from the popup
-    const date = new Date();
-    console.log(`Saving [ ${date} , ${customString}]`);
-
-    window.electronAPI.addRecord(date, customString);
-}
-
-//function deleteButtonEventListener() {
-    //window.electronAPI.deleteRecord(recordId); //TODO: Pending implementation 
-//}
-
-
-saveButton.addEventListener('click', saveButtonEventListener);
-//deleteButton.addEventListener('click', deleteButtonEventListener);
 // Listen for data sent from the main process
 window.electronAPI.onOpenPopupData((event, { date, text }) => {
+    console.log('Will show popup with info...')
     displayRecord(date, text);
 });
 
 // Function to display the received record data
 function displayRecord(date, text) {
+    console.log('Data to show:' + date + ' - ' + text)
     dateTextField.value = date;
     dataTextField.value = text;
 }
