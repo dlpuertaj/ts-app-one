4b27fa7e0ea730aa5cff57b1a185469bf5906810
David Leonardo <dlpuerta@gmail.com>
2024-11-04 21:55:11 -0500

    Fixing undefined value bug

--- src/preload.js
+++ src/preload.js
@@ -4,6 +4,6 @@ contextBridge.exposeInMainWorld('electronAPI', {
     getRecords: () => ipcRenderer.invoke('get-records'),
     addRecord: (dateTime, text) => ipcRenderer.invoke('add-record',dateTime, text ),
     onOpenPopupData: (callback) => ipcRenderer.on('open-popup-data',callback),
-    openPopup: (date,text) => ipcRenderer.send('open-popup-window',date,text),
+    openPopup: (id, date, text) => ipcRenderer.send('open-popup-window',id, date,text),
     updateRecord: (id,date,text) => ipcRenderer.invoke('update-record', id, date, text)
 });
